8fbe7d60567caf082dbd1c1f18f55d6165662f21
Wesley Ricardo <wesleyricardo.ls@hotmail.com>
2019-02-20 21:01:24 -0300

    Add project files.

---
+++ src/SGC.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace SGC.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}

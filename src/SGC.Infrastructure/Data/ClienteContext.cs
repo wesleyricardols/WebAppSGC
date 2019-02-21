250225bc9df14052511d99cf24a532461a1a7be6
Wesley Ricardo <wesleyricardo.ls@hotmail.com>
2019-02-20 22:19:41 -0300

    Cria a classe ClienteContext e adiciona migrations

---
+++ src/SGC.Infrastructure/Data/ClienteContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGC.ApplicationCore.Entity;
+
+namespace SGC.Infrastructure.Data
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().ToTable("Tb_Cliente");
+        }
+    }
+}
---
+++ src/SGC.Infrastructure/Migrations/20190221010911_Inicial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SGC.Infrastructure.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tb_Cliente",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    CPF = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tb_Cliente", x => x.ClienteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tb_Cliente");
+        }
+    }
+}

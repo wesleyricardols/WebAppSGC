ac53f78fc7d2050719a7cfbbd7209cebf52976ed
Wesley Ricardo <wesleyricardo.ls@hotmail.com>
2019-02-25 00:23:14 -0300

    Adiciona especialidades das entidades para Repository e implementa ClienteService e ContatoService

---
+++ src/SGC.ApplicationCore/Services/ClienteService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using SGC.ApplicationCore.Entity;
+using SGC.ApplicationCore.Interfaces.Repository;
+using SGC.ApplicationCore.Interfaces.Services;
+
+namespace SGC.ApplicationCore.Services
+{
+    public class ClienteService : IClienteService
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteService(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        public Cliente Adicionar(Cliente entity)
+        {
+            //TODO: Adicionar regra de negócio antes de 
+            return _clienteRepository.Adicionar(entity);
+        }
+
+        public void Atualizar(Cliente entity)
+        {
+            _clienteRepository.Atualizar(entity);
+        }
+
+        public IEnumerable<Cliente> Buscar(Expression<Func<Cliente, bool>> predicado)
+        {
+            return _clienteRepository.Buscar(predicado);
+        }
+
+        public Cliente ObterPorId(int id)
+        {
+            return _clienteRepository.ObterPorId(id);
+        }
+
+        public IEnumerable<Cliente> ObterTodos()
+        {
+            return _clienteRepository.ObterTodos();
+        }
+
+        public void Remover(Cliente entity)
+        {
+            _clienteRepository.Remover(entity);
+        }
+    }
+}
---
+++ src/SGC.ApplicationCore/Services/ContatoService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using SGC.ApplicationCore.Entity;
+using SGC.ApplicationCore.Interfaces.Repository;
+using SGC.ApplicationCore.Interfaces.Services;
+
+namespace SGC.ApplicationCore.Services
+{
+    public class ContatoService : IContatoService
+    {
+        private readonly IContatoRepository _contatoService;
+
+        public ContatoService(IContatoRepository contatoRepository)
+        {
+            _contatoService = contatoRepository;
+        }
+
+        public Contato Adicionar(Contato entity)
+        {
+            return _contatoService.Adicionar(entity);
+        }
+
+        public void Atualizar(Contato entity)
+        {
+            _contatoService.Atualizar(entity);
+        }
+
+        public IEnumerable<Contato> Buscar(Expression<Func<Contato, bool>> predicado)
+        {
+            return _contatoService.Buscar(predicado);
+        }
+
+        public Contato ObterPorId(int id)
+        {
+            return _contatoService.ObterPorId(id);
+        }
+
+        public IEnumerable<Contato> ObterTodos()
+        {
+            return _contatoService.ObterTodos();
+        }
+
+        public void Remover(Contato entity)
+        {
+            _contatoService.Remover(entity);
+        }
+    }
+}
